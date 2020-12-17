6b0d2f364a327a8ac1fe21eb8cff6594f3c680bd
Joel Machango <joelmachango@gmail.com>
2020-12-17 07:29:42 +0300

    Add grant unit tests

--- src/app/grant/grant-add/grant-add.component.spec.ts
+++ src/app/grant/grant-add/grant-add.component.spec.ts
@@ -1,4 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+
+import { BrowserModule, By } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { DebugElement } from '@angular/core';
 
 import { GrantAddComponent } from './grant-add.component';
 
@@ -6,20 +11,68 @@ describe('GrantAddComponent', () => {
   let component: GrantAddComponent;
   let fixture: ComponentFixture<GrantAddComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ GrantAddComponent ]
-    })
-    .compileComponents();
-  });
+  let de: DebugElement;
+  let el: HTMLElement;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GrantAddComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [GrantAddComponent],
+      imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(GrantAddComponent);
+
+        component = fixture.componentInstance; // ContactComponent test instance
+        // query for the title <h1> by CSS element selector
+        de = fixture.debugElement.query(By.css('form'));
+        el = de.nativeElement;
+      });
+  }));
+
+  // beforeEach(async () => {
+  //   await TestBed.configureTestingModule({
+  //     declarations: [GrantAddComponent],
+  //   }).compileComponents();
+  // });
+
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(GrantAddComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`should have as text 'contact page'`, async(() => {
+    expect(component.text).toEqual('contact page');
+  }));
+
+  it(`should set submitted to true`, async(() => {
+    component.onSubmit();
+    expect(component.submitted).toBeTruthy();
+  }));
+
+  // it(`should call the onSubmit method`, async(() => {
+  //   spyOn(component, 'onSubmit');
+  //   el = fixture.debugElement.query(By.css('button')).nativeElement;
+  //   el.click();
+  //   expect(component.onSubmit).toHaveBeenCalled();
+  // }));
+
+  it(`form should be invalid`, async(() => {
+    component.contactForm.controls['email'].setValue('');
+    component.contactForm.controls['name'].setValue('');
+    component.contactForm.controls['text'].setValue('');
+    expect(component.contactForm.valid).toBeFalsy();
+  }));
+
+  it(`form should be valid`, async(() => {
+    component.contactForm.controls['email'].setValue('asd@asd.com');
+    component.contactForm.controls['name'].setValue('aada');
+    component.contactForm.controls['text'].setValue('text');
+    expect(component.contactForm.valid).toBeTruthy();
+  }));
 });
